1ce00a75fc73ce137b8af41a49679810acfad4d9
Koz Ross <koz.ross@retro-freedom.nz>
2022-05-13 13:43:14 +1200

    Add rest of minting context construction API

--- src/Plutarch/Context/Minting.hs
+++ src/Plutarch/Context/Minting.hs
@@ -12,13 +12,26 @@ module Plutarch.Context.Minting (
     -- ** Construction
     mkTokens,
 
+    -- ** Describe inputs
+    inputFromPubKey,
+    inputFromPubKeyWith,
+    inputTokensFromPubKey,
+    inputTokensFromPubKeyWith,
+    inputFromOtherScript,
+    inputTokensFromOtherScript,
+
     -- ** Describe outputs
-    outputValueToPubKey,
-    outputTokensToPubKey,
+    outputToPubKey,
     outputToPubKeyWith,
+    outputTokensToPubKey,
     outputTokensToPubKeyWith,
     outputToOtherScript,
     outputTokensToOtherScript,
+
+    -- ** Describe others
+    mintForeign,
+    signedWith,
+    extraData,
 ) where
 
 import Control.Monad (guard)
@@ -27,6 +40,7 @@ import Data.Kind (Type)
 import Plutarch (S)
 import Plutarch.Builtin (PIsData, pdata, pforgetData)
 import Plutarch.Context.Internal (
+    Minting (Minting),
     MintingBuilder (MB),
     SideUTXO (SideUTXO),
     UTXOType (PubKeyUTXO, ScriptUTXO),
@@ -51,18 +65,6 @@ data Tokens = T TokenName Integer
           Show
         )
 
-{- | Describes what we want a minting policy to do.
-
- @since 1.0.0
--}
-data MintingAction = Mint Tokens | Burn Tokens
-    deriving stock
-        ( -- | @since 1.0.0
-          Eq
-        , -- | @since 1.0.0
-          Show
-        )
-
 {- | Attempt to make a 'Tokens' with the given name and amount. Fails if given a
  non-positive amount.
 
@@ -80,16 +82,125 @@ pattern Tokens name amount <- T name amount
 
 {-# COMPLETE Tokens #-}
 
-{- | Describes a single output to a 'PubKey'.
+{- | Describes what we want a minting policy to do.
 
  @since 1.0.0
 -}
-outputValueToPubKey ::
+data MintingAction = Mint Tokens | Burn Tokens
+    deriving stock
+        ( -- | @since 1.0.0
+          Eq
+        , -- | @since 1.0.0
+          Show
+        )
+
+{- | Describes a single input from a 'PubKey'.
+
+ @since 1.0.0
+-}
+inputFromPubKey ::
     forall (redeemer :: Type).
     PubKeyHash ->
     Value ->
     MintingBuilder redeemer
-outputValueToPubKey pkh val = output go
+inputFromPubKey pkh val = input go
+  where
+    go :: SideUTXO
+    go = SideUTXO (PubKeyUTXO pkh Nothing) (GeneralValue val)
+
+{- | Describes a single input from a 'PubKey', where some additional data is
+ also provided.
+
+ @since 1.0.0
+-}
+inputFromPubKeyWith ::
+    forall (a :: Type) (redeemer :: Type) (p :: S -> Type).
+    (PUnsafeLiftDecl p, PLifted p ~ a, PIsData p) =>
+    PubKeyHash ->
+    Value ->
+    a ->
+    MintingBuilder redeemer
+inputFromPubKeyWith pkh val x = input go
+  where
+    go :: SideUTXO
+    go = SideUTXO (PubKeyUTXO pkh . Just . datafy $ x) (GeneralValue val)
+
+{- | Describes a single input of 'Tokens' from a 'PubKey'.
+
+ @since 1.0.0
+-}
+inputTokensFromPubKey ::
+    forall (redeemer :: Type).
+    PubKeyHash ->
+    Tokens ->
+    MintingBuilder redeemer
+inputTokensFromPubKey pkh (T name amount) = input go
+  where
+    go :: SideUTXO
+    go = SideUTXO (PubKeyUTXO pkh Nothing) (TokensValue name amount)
+
+{- | As 'inputTokensFromPubKey', but with some extra data as well.
+
+ @since 1.0.0
+-}
+inputTokensFromPubKeyWith ::
+    forall (a :: Type) (redeemer :: Type) (p :: S -> Type).
+    (PUnsafeLiftDecl p, PLifted p ~ a, PIsData p) =>
+    PubKeyHash ->
+    Tokens ->
+    a ->
+    MintingBuilder redeemer
+inputTokensFromPubKeyWith pkh (T name amount) x = input go
+  where
+    go :: SideUTXO
+    go =
+        let val = TokensValue name amount
+            typ = PubKeyUTXO pkh . Just . datafy $ x
+         in SideUTXO typ val
+
+{- | Describes an input from a script /other/ than the one for which the context
+ is being made.
+
+ @since 1.0.0
+-}
+inputFromOtherScript ::
+    forall (a :: Type) (redeemer :: Type) (p :: S -> Type).
+    (PUnsafeLiftDecl p, PLifted p ~ a, PIsData p) =>
+    ValidatorHash ->
+    Value ->
+    a ->
+    MintingBuilder redeemer
+inputFromOtherScript vh val x = input go
+  where
+    go :: SideUTXO
+    go = SideUTXO (ScriptUTXO vh . datafy $ x) (GeneralValue val)
+
+{- | As 'inputFromOtherScript', except the input is a 'Tokens'.
+
+ @since 1.0.0
+-}
+inputTokensFromOtherScript ::
+    forall (a :: Type) (redeemer :: Type) (p :: S -> Type).
+    (PUnsafeLiftDecl p, PLifted p ~ a, PIsData p) =>
+    ValidatorHash ->
+    Tokens ->
+    a ->
+    MintingBuilder redeemer
+inputTokensFromOtherScript vh (T name amount) x = input go
+  where
+    go :: SideUTXO
+    go = SideUTXO (ScriptUTXO vh . datafy $ x) (TokensValue name amount)
+
+{- | Describes a single output to a 'PubKey'.
+
+ @since 1.0.0
+-}
+outputToPubKey ::
+    forall (redeemer :: Type).
+    PubKeyHash ->
+    Value ->
+    MintingBuilder redeemer
+outputToPubKey pkh val = output go
   where
     go :: SideUTXO
     go = SideUTXO (PubKeyUTXO pkh Nothing) (GeneralValue val)
@@ -110,7 +221,7 @@ outputTokensToPubKey pkh (T name amount) = output go
         let val = TokensValue name amount
          in SideUTXO (PubKeyUTXO pkh Nothing) val
 
-{- | As 'outputValueToPubKey', but adds some extra data as well.
+{- | As 'outputToPubKey', but adds some extra data as well.
 
  @since 1.0.0
 -}
@@ -184,7 +295,45 @@ outputTokensToOtherScript vh (T name amount) x = output go
             typ = ScriptUTXO vh . datafy $ x
          in SideUTXO typ value
 
+{- | Describe that we've signed with one signature.
+
+ @since 1.0.0
+-}
+signedWith ::
+    forall (redeemer :: Type).
+    PubKeyHash ->
+    MintingBuilder redeemer
+signedWith pkh = MB mempty mempty (pure pkh) mempty mempty
+
+{- | Describe a mint of tokens /not/ controlled by the minting policy for which
+ this script context is being made.
+
+ @since 1.0.0
+-}
+mintForeign ::
+    forall (redeemer :: Type).
+    Value ->
+    MintingBuilder redeemer
+mintForeign = MB mempty mempty mempty mempty . pure . Minting
+
+{- | Describe that there is an additional piece of data in the context.
+
+ @since 1.0.0
+-}
+extraData ::
+    forall (a :: Type) (redeemer :: Type) (p :: S -> Type).
+    (PUnsafeLiftDecl p, PLifted p ~ a, PIsData p) =>
+    a ->
+    MintingBuilder redeemer
+extraData x = MB mempty mempty mempty (pure . datafy $ x) mempty
+
 -- Helpers
+
+input ::
+    forall (redeemer :: Type).
+    SideUTXO ->
+    MintingBuilder redeemer
+input x = MB (pure x) mempty mempty mempty mempty
 
 output ::
     forall (redeemer :: Type).
@@ -198,43 +347,3 @@ datafy ::
     a ->
     Data
 datafy x = plift (pforgetData (pdata (pconstant x)))
-
-{-
--- | Describe a single input from a 'SideUTXO'.
---
--- @since 1.0.0
-input :: forall (redeemer :: Type) .
-  SideUTXO ->
-  MintingBuilder redeemer
-input x = MB (pure x) mempty mempty mempty mempty
-
--- | Describe a single output to a 'SideUTXO'.
---
--- @since 1.0.0
-
--- | Describe that we've signed with one signature.
---
--- @since 1.0.0
-signedWith :: forall (redeemer :: Type) .
-  PubKeyHash ->
-  MintingBuilder redeemer
-signedWith pkh = MB mempty mempty (pure pkh) mempty mempty
-
--- | Describe that there is an additional piece of data in the context.
---
--- @since 1.0.0
-extraData :: forall (a :: Type) (redeemer :: Type) (p :: S -> Type) .
-  (PUnsafeLiftDecl p, PLifted p ~ a, PIsData p) =>
-  a ->
-  MintingBuilder redeemer
-extraData x = MB mempty mempty mempty (pure . datafy $ x) mempty
-
--- | Describe that something other than the script we're using this context with
--- mints a value.
---
--- @since 1.0.0
-otherMint :: forall (redeemer :: Type) .
-  Minting ->
-  MintingBuilder redeemer
-otherMint m = MB mempty mempty mempty mempty (pure m)
--}
