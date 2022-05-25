ec8da863740301da948c402011a6ebaa070462f2
Koz Ross <koz.ross@retro-freedom.nz>
2022-05-25 13:11:17 +1200

    Add PState, PAssetClass

---
+++ src/Plutarch/Api/V1/AssetClass.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Plutarch.Api.V1.AssetClass (
+    PAssetClass (..),
+    passetClass,
+    passetClassValueOf,
+    pvalueOf,
+) where
+
+import Data.Kind (Type)
+import Generics.SOP (I (I))
+import Generics.SOP.TH (deriveGeneric)
+import Plutarch (
+    PlutusType,
+    S,
+    Term,
+    pcon,
+    phoistAcyclic,
+    plam,
+    unTermCont,
+    (#),
+    type (:-->),
+ )
+import Plutarch.Api.V1.AssocMap (PMap (PMap))
+import Plutarch.Api.V1.Value (
+    PCurrencySymbol,
+    PTokenName,
+    PValue (PValue),
+ )
+import Plutarch.Bool (PBool, PEq ((#==)))
+import Plutarch.Builtin (
+    PAsData,
+    PBuiltinPair,
+    PIsData,
+    pdata,
+    pfromData,
+    pfstBuiltin,
+    psndBuiltin,
+ )
+import Plutarch.DataRepr (
+    PDataFields,
+    PDataRecord,
+    PIsDataRepr,
+    PIsDataReprInstances (PIsDataReprInstances),
+    PLabeledType ((:=)),
+    pdcons,
+    pdnil,
+    pfield,
+ )
+import Plutarch.Extra.TermCont (pletC, pmatchC)
+import Plutarch.Integer (PInteger)
+import Plutarch.List (pfind)
+import Plutarch.Maybe (PMaybe (PJust, PNothing))
+
+-- | @since 1.0.0
+newtype PAssetClass (s :: S)
+    = PAssetClass
+        ( Term
+            s
+            ( PDataRecord
+                '[ "currencySymbol" ':= PCurrencySymbol
+                 , "tokenName" ':= PTokenName
+                 ]
+            )
+        )
+
+deriveGeneric ''PAssetClass
+
+-- | @since 1.0.0
+deriving anyclass instance PIsDataRepr PAssetClass
+
+-- | @since 1.0.0
+deriving via (PIsDataReprInstances PAssetClass) instance (PDataFields PAssetClass)
+
+-- | @since 1.0.0
+deriving via (PIsDataReprInstances PAssetClass) instance (PIsData PAssetClass)
+
+-- | @since 1.0.0
+deriving via (PIsDataReprInstances PAssetClass) instance (PlutusType PAssetClass)
+
+-- | @since 1.0.0
+deriving via (PIsDataReprInstances PAssetClass) instance (PEq PAssetClass)
+
+-- | @since 1.0.0
+passetClass ::
+    forall (s :: S).
+    Term s (PCurrencySymbol :--> PTokenName :--> PAssetClass)
+passetClass = phoistAcyclic $
+    plam $ \cs tn ->
+        pcon . PAssetClass $ pdcons # pdata cs # (pdcons # pdata tn # pdnil)
+
+-- | @since 1.0.0
+passetClassValueOf ::
+    forall (s :: S).
+    Term s (PValue :--> PAssetClass :--> PInteger)
+passetClassValueOf = phoistAcyclic $
+    plam $ \val ac -> unTermCont $ do
+        cs <- pletC (pfromData $ pfield @"currencySymbol" # ac)
+        tn <- pletC (pfromData $ pfield @"tokenName" # ac)
+        pure $ pvalueOf # val # cs # tn
+
+-- | @since 1.0.0
+pvalueOf ::
+    forall (s :: S).
+    Term s (PValue :--> PCurrencySymbol :--> PTokenName :--> PInteger)
+pvalueOf = phoistAcyclic $
+    plam $ \val cs tn -> unTermCont $ do
+        PValue m <- pmatchC val
+        PMap m' <- pmatchC m
+        res <- pmatchC (pfind # (go # cs) # m')
+        case res of
+            PNothing -> pure 0
+            PJust inner -> do
+                PMap innerMap <- pmatchC (pfromData $ psndBuiltin # inner)
+                res2 <- pmatchC (pfind # (go # tn) # innerMap)
+                case res2 of
+                    PNothing -> pure 0
+                    PJust answer -> pure . pfromData $ psndBuiltin # answer
+  where
+    go ::
+        forall (a :: S -> Type) (b :: S -> Type) (s' :: S).
+        (PIsData a, PEq a) =>
+        Term s' (a :--> PBuiltinPair (PAsData a) (PAsData b) :--> PBool)
+    go = phoistAcyclic $
+        plam $ \target p -> unTermCont $ do
+            a' <- pletC (pfromData $ pfstBuiltin # p)
+            pure (target #== a')
---
+++ src/Plutarch/Extra/State.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Plutarch.Extra.State (
+    PState,
+    pstate,
+    prunState,
+    pevalState,
+    pexecState,
+    pget,
+    pput,
+    pmodify,
+) where
+
+import Data.Kind (Type)
+import Generics.SOP (Top)
+import Plutarch (
+    DerivePNewtype (DerivePNewtype),
+    PlutusType,
+    S,
+    Term,
+    pcon,
+    phoistAcyclic,
+    plam,
+    unTermCont,
+    (#),
+    type (:-->),
+ )
+import Plutarch.Extra.Applicative (PApplicative (ppure), PApply (pliftA2))
+import Plutarch.Extra.Functor (PFunctor (PSubcategory, pfmap))
+import Plutarch.Extra.TermCont (pmatchC)
+import Plutarch.Pair (PPair (PPair))
+import Plutarch.Unit (PUnit (PUnit))
+
+-- | @since 1.0.0
+newtype PState (s :: S -> Type) (a :: S -> Type) (s' :: S)
+    = PState (Term s' (s :--> PPair s a))
+    deriving
+        ( -- | @since 1.0.0
+          PlutusType
+        )
+        via (DerivePNewtype (PState s a) (s :--> PPair s a))
+
+-- | @since 1.0.0
+instance PFunctor (PState s) where
+    type PSubcategory (PState s) = Top
+    pfmap = phoistAcyclic $
+        plam $ \f state -> unTermCont $ do
+            PState g <- pmatchC state
+            pure . pcon . PState $ plam $ \s -> pfmap # f # (g # s)
+
+-- | @since 1.0.0
+instance PApply (PState s) where
+    pliftA2 = phoistAcyclic $
+        plam $ \f xs ys -> unTermCont $ do
+            PState g <- pmatchC xs
+            PState h <- pmatchC ys
+            pure . pcon . PState $
+                plam $ \s -> unTermCont $ do
+                    PPair s' x <- pmatchC (g # s)
+                    PPair s'' y <- pmatchC (h # s')
+                    pure . pcon . PPair s'' $ f # x # y
+
+-- | @since 1.0.0
+instance PApplicative (PState s) where
+    ppure =
+        phoistAcyclic $ plam $ \x -> pcon . PState $ plam $ \s -> pcon . PPair s $ x
+
+{- | Lift a Plutarch lambda into 'PState'.
+
+ @since 1.0.0
+-}
+pstate ::
+    forall (s :: S -> Type) (a :: S -> Type) (s' :: S).
+    Term s' ((s :--> PPair s a) :--> PState s a)
+pstate = phoistAcyclic $ plam $ pcon . PState
+
+-- | @since 1.0.0
+prunState ::
+    forall (s :: S -> Type) (a :: S -> Type) (s' :: S).
+    Term s' (PState s a :--> s :--> PPair s a)
+prunState = phoistAcyclic $
+    plam $ \comp state -> unTermCont $ do
+        PState f <- pmatchC comp
+        pure $ f # state
+
+-- | @since 1.0.0
+pevalState ::
+    forall (s :: S -> Type) (a :: S -> Type) (s' :: S).
+    Term s' (PState s a :--> s :--> a)
+pevalState = phoistAcyclic $
+    plam $ \comp state -> unTermCont $ do
+        PPair _ x <- pmatchC (prunState # comp # state)
+        pure x
+
+-- | @since 1.0.0
+pexecState ::
+    forall (s :: S -> Type) (a :: S -> Type) (s' :: S).
+    Term s' (PState s a :--> s :--> s)
+pexecState = phoistAcyclic $
+    plam $ \comp state -> unTermCont $ do
+        PPair state' _ <- pmatchC (prunState # comp # state)
+        pure state'
+
+-- | @since 1.0.0
+pget ::
+    forall (s :: S -> Type) (s' :: S).
+    Term s' (PState s s)
+pget = pcon . PState $ plam $ \s -> pcon . PPair s $ s
+
+-- | @since 1.0.0
+pput ::
+    forall (s :: S -> Type) (s' :: S).
+    Term s' (s :--> PState s PUnit)
+pput = phoistAcyclic $ plam $ \x -> pcon . PState $ plam $ \_ -> pcon . PPair x . pcon $ PUnit
+
+-- | @since 1.0.0
+pmodify ::
+    forall (s :: S -> Type) (s' :: S).
+    Term s' ((s :--> s) :--> PState s PUnit)
+pmodify = phoistAcyclic $ plam $ \f -> pcon . PState $ plam $ \s -> pcon . PPair (f # s) . pcon $ PUnit
