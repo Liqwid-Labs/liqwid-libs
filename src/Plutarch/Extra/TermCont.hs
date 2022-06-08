67ebdbc604640f5ed6034542c3fcdb4f985c3fad
Hongrui Fang <chfanghr@gmail.com>
2022-06-08 21:14:59 +0800

    add `passertC`

--- src/Plutarch/Extra/TermCont.hs
+++ src/Plutarch/Extra/TermCont.hs
@@ -1,4 +1,10 @@
-module Plutarch.Extra.TermCont (module Extra) where
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Plutarch.Extra.TermCont (
+    passertC,
+    module Extra,
+) where
 
 import "plutarch-extra" Plutarch.Extra.TermCont as Extra (
     pguardC,
@@ -9,3 +15,24 @@ import "plutarch-extra" Plutarch.Extra.TermCont as Extra (
     ptraceC,
     ptryFromC,
  )
+
+import Plutarch (
+    S,
+    Term,
+ )
+import Plutarch.Bool (PBool, pif)
+import Plutarch.String (PString)
+import Plutarch.TermCont (TermCont, tcont)
+import Plutarch.Trace (ptraceError)
+
+{- | Assert a particular 'PBool', trace if false.
+
+    @since 1.0.0
+-}
+passertC ::
+    forall r (s :: S).
+    Term s PString ->
+    Term s PBool ->
+    TermCont @r s ()
+passertC errorMessage check = tcont $ \k ->
+    pif check (k ()) (ptraceError errorMessage)
