b3d9dda05118de27a7a4a7a25152cb25a7412a9a
Hongrui Fang <chfanghr@gmail.com>
2022-06-14 22:41:34 +0800

    remove `passertC`
    This reverts commit 67ebdbc604640f5ed6034542c3fcdb4f985c3fad.

--- src/Plutarch/Extra/TermCont.hs
+++ src/Plutarch/Extra/TermCont.hs
@@ -1,10 +1,4 @@
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE TypeApplications #-}
-
-module Plutarch.Extra.TermCont (
-    passertC,
-    module Extra,
-) where
+module Plutarch.Extra.TermCont (module Extra) where
 
 import "plutarch-extra" Plutarch.Extra.TermCont as Extra (
     pguardC,
@@ -15,24 +9,3 @@ import "plutarch-extra" Plutarch.Extra.TermCont as Extra (
     ptraceC,
     ptryFromC,
  )
-
-import Plutarch (
-    S,
-    Term,
- )
-import Plutarch.Bool (PBool, pif)
-import Plutarch.String (PString)
-import Plutarch.TermCont (TermCont, tcont)
-import Plutarch.Trace (ptraceError)
-
-{- | Assert a particular 'PBool', trace if false.
-
-    @since 1.0.0
--}
-passertC ::
-    forall r (s :: S).
-    Term s PString ->
-    Term s PBool ->
-    TermCont @r s ()
-passertC errorMessage check = tcont $ \k ->
-    pif check (k ()) (ptraceError errorMessage)
