c53609f9c5e2b41a52ea01f858a041cefdd460ce
Johannes Krause <johannes@mlabs.city>
2022-07-04 19:15:15 +0200

    (split)Benchmarking: Simplified to pure gen interface, added pure QC adapter

---
+++ Test/Benchmark/QuickCheck.hs
@@ -0,0 +1,35 @@
+{- | Bridge between QuickCheck generators and System.Random
+
+ Inspired by hedgehog-quickcheck.
+-}
+module Test.Benchmark.QuickCheck (
+  genToRand,
+  randToGen,
+) where
+
+import System.Random (RandomGen, mkStdGen)
+import System.Random.Stateful (Uniform (uniformM), runStateGen)
+import Test.QuickCheck (arbitrary)
+import Test.QuickCheck.Gen (Gen (MkGen), sized)
+import Test.QuickCheck.Random (mkQCGen)
+
+-- | Converts a QuickCheck Gen to a pure function in terms of System.Random.RandomGen
+genToRand ::
+  forall (a :: Type) (g :: Type).
+  RandomGen g =>
+  Gen a ->
+  Int ->
+  g ->
+  (a, g)
+genToRand (MkGen runQcGen) size g = runStateGen g $ \stateGen -> do
+  qcGen <- mkQCGen <$> uniformM stateGen
+  pure $ runQcGen qcGen size
+
+-- | Converts a pure function in terms of System.Random.RandomGen to a QuickCheck Gen
+randToGen ::
+  forall (a :: Type).
+  (forall (g :: Type). RandomGen g => Int -> g -> (a, g)) ->
+  Gen a
+randToGen pureRand = do
+  g <- mkStdGen <$> arbitrary
+  sized $ \size -> pure $ fst $ pureRand size g
--- Test/Benchmark/Sized.hs
+++ Test/Benchmark/Sized.hs
@@ -5,24 +5,19 @@ module Test.Benchmark.Sized (
   SSample (..),
   DomainCardinality (..),
   SDomainGen (..),
-  mkSDomainPureSTGen,
-  mkSDomainSTGen,
-  sizedPureRandomGen,
   benchSizes,
 ) where
 
 import Control.Monad (filterM)
-import Control.Monad.Reader (MonadReader (ask), ReaderT)
-import Control.Monad.ST (ST)
 import Control.Monad.ST.Class (MonadST, liftST)
-import Control.Monad.Trans (lift)
+import Control.Monad.State.Strict (StateT)
 import Data.HashTable.ST.Basic qualified as HashTable
 import Data.Hashable (Hashable)
 import Data.Maybe (isNothing)
 import GHC.Generics (Generic)
 import Numeric.Natural (Natural)
-import System.Random (RandomGen, mkStdGen)
-import System.Random.Stateful (RandomGenM (applyRandomGenM), STGenM, applySTGen, newSTGenM)
+import System.Random (RandomGen, StdGen, mkStdGen)
+import System.Random.Stateful (StateGenM, applyRandomGenM, runStateGenT_, uniformRM)
 
 -- | Holds sample and metadata for a certain input size
 data SSample s = SSample
@@ -44,7 +39,7 @@ data DomainCardinality
 
  Holds everything needed to generate the set of inputs for a given input size.
 -}
-data SDomainGen (m :: Type -> Type) (a :: Type) = SDomainGen
+data SDomainGen (a :: Type) = SDomainGen
   { cardinalityOfSize :: Int -> DomainCardinality
   -- ^ Number of possible inputs of a given input size
   --
@@ -56,88 +51,17 @@ data SDomainGen (m :: Type -> Type) (a :: Type) = SDomainGen
   -- non-termination.
   , exhaustiveGen :: Int -> [a]
   -- ^ Exhaustive input generator, given the input size
-  , randomGen :: Int -> m a
+  , randomGen :: forall (g :: Type). RandomGen g => Int -> g -> (a, g)
   -- ^ Random input generator, given the input size.
   --
-  -- You can use 'Test.QuickCheck.generate :: Gen a -> IO a' here.
-  -- You need to keep the your random seed state in the monadic context,
-  -- see also 'sizedPureRandomGen'.
-  , indexGen :: Int -> m Int
-  -- ^ A way to generate random indices in @m@, from 0 to <arg> inclusive.
+  -- For a comfortable interface, use something like 'System.Random.Stateful.runStateGen'
+  -- or the MonadRandom package.
   --
-  -- When @desiredSampleSize / cardinality > 0.5@, 'exhaustiveGen' is used.
-  -- 'indexGen' gets used to generate indices to drop from 'exhaustiveGen' output.
+  -- See 'Test.Benchmark.QuickCheck' for how to use QuickCheck generators here.
+  -- You probably want either an uniform distribution, or a distribution that
+  -- represents real-world inputs. Keep that in mind when reusing QuickCheck
+  -- generators, they might not have been written with that in mind.
   }
-
--- | Make SDomainGen based on STGenM. Random seed originates here.
-mkSDomainSTGen ::
-  forall m a.
-  ( Monad m
-  , MonadST m
-  ) =>
-  (Int -> DomainCardinality) ->
-  (Int -> [a]) ->
-  (forall (g :: Type) (s :: Type). RandomGen g => Int -> STGenM g s -> ST s a) ->
-  (Int -> m Int) ->
-  m (SDomainGen m a)
-mkSDomainSTGen cardinalityOfSize exhaustiveGen stRandomGen indexGen = do
-  g <- liftST $ newSTGenM (mkStdGen 42)
-  let randomGen inputSize = liftST $ stRandomGen inputSize g
-  pure $ SDomainGen {cardinalityOfSize, exhaustiveGen, randomGen, indexGen}
-
--- | Make SDomainGen using a pure random gen. Random seed originates here.
-mkSDomainPureSTGen ::
-  forall m a.
-  ( Monad m
-  , MonadST m
-  ) =>
-  (Int -> DomainCardinality) ->
-  (Int -> [a]) ->
-  (forall (g :: Type). RandomGen g => Int -> g -> (a, g)) ->
-  (Int -> m Int) ->
-  m (SDomainGen m a)
-mkSDomainPureSTGen cardinalityOfSize exhaustiveGen pureRandomGen =
-  mkSDomainSTGen cardinalityOfSize exhaustiveGen (applySTGen . pureRandomGen)
-
-{- | Adapt a size-parametrized pure random gen for use in 'SSample'.
-
-Solves the problem of keeping the random seed state var (like 'STGenM')
-in a monadic context.
-
-Usage with IO:
-@
-let gen = SDomainGen _ _ (sizedPureRandomGen $ \n -> genWord8)
-g <- newIOGenM (mkStdGen 42)
-ssamples <- runReaderT (benchSizes gen _sampleFun _inputs _desiredSamplesPerInput) g
-print ssamples
-@
-
-Usage with ST:
-@
-let gen = SDomainGen _ _ (sizedPureRandomGen $ \n -> genWord8)
-g <- liftST $ newSTGenM (mkStdGen 42)
-ssamples <- liftST $ runReaderT (benchSizes gen _sampleFun _inputs _desiredSamplesPerInput) g
-print ssamples
-@
-
-TODO Could use 'MonadRandom', but that is not exception-safe. Not sure if
-  exception-safety will be needed.
--}
-sizedPureRandomGen ::
-  forall (g :: Type) (r :: Type) (m :: Type -> Type) (a :: Type).
-  ( RandomGenM g r m
-  , MonadST m
-  ) =>
-  (Int -> r -> (a, r)) ->
-  Int ->
-  ReaderT g m a
-sizedPureRandomGen pureRandomGen size =
-  ask >>= lift . applyRandomGenM (pureRandomGen size)
-
--- TODO Should probably just go with a MonadRandom and write an adapter for QuickCheck stuff
--- that works like in
--- https://hackage.haskell.org/package/hedgehog-quickcheck-0.1.1/docs/src/Hedgehog.Gen.QuickCheck.html
--- -> Would have known way to save the seed before a crash, or to continue after ctrl+c
 
 {- | Benchmark for various input sizes. Handles small input sizes correctly.
 
@@ -148,17 +72,19 @@ sizedPureRandomGen pureRandomGen size =
  TODO An actual Stream might be a better choice
 -}
 benchSizes ::
-  forall (a :: Type) (m :: Type -> Type) (s :: Type).
+  forall (a :: Type) (m :: Type -> Type) (se :: Type).
   ( Eq a
   , Ord a
   , Show a -- TODO print input when exception happens
   , Hashable a
+  -- TODO could hide that ST is being used, but need effects anyway for displaying progress later
+  --   so at least a Monad constraint will probably be involved
   , MonadST m
   ) =>
   -- | Size-dependent input domain generator.
-  SDomainGen m a ->
-  -- | Sampling function.
-  (a -> s) ->
+  SDomainGen a ->
+  -- | Sampling function: From input to sample element.
+  (a -> se) ->
   -- | The input sizes to benchmark with. Usually something like @[0..n]@.
   [Int] ->
   -- | Desired number of samples per input size.
@@ -166,21 +92,21 @@ benchSizes ::
   -- The actual number of samples will be exactly
   -- @min (cardinalityOfSize inputSize) desiredSamplesPerInputSize@
   Int ->
-  m [SSample [s]]
+  m [SSample [se]]
 benchSizes
   SDomainGen
     { cardinalityOfSize
     , randomGen
     , exhaustiveGen
-    , indexGen
     }
   sampleFun
   sizes
   desiredSamplesPerInputSize =
-    mapM benchInputSize sizes
+    -- using StateGenM to be able to freeze the seed. MonadRandom can't do this..
+    runStateGenT_ (mkStdGen 42) $ \g -> mapM (benchInputSize g) sizes
     where
-      benchInputSize :: Int -> m (SSample [s])
-      benchInputSize inputSize = do
+      benchInputSize :: StateGenM StdGen -> Int -> StateT StdGen m (SSample [se])
+      benchInputSize g inputSize = do
         inputs <- genInputs
         let sample = fmap sampleFun inputs
         pure $ SSample {inputSize, coverage, sampleSize = numInputs, sample}
@@ -191,7 +117,7 @@ benchSizes
               Just $ fromIntegral numInputs / fromIntegral card
           cardinality = cardinalityOfSize inputSize
           (numInputs, genInputs) = case cardinality of
-            HugeDomainCardinality -> (desiredSamplesPerInputSize, genRandom)
+            HugeDomainCardinality -> (desiredSamplesPerInputSize, genRandom g)
             DomainCardinality card ->
               if card <= fromIntegral desiredSamplesPerInputSize
                 then
@@ -201,18 +127,18 @@ benchSizes
                 else
                   ( desiredSamplesPerInputSize
                   , if coverage > Just 0.5
-                      then genRandomSubset card
-                      else genRandom
+                      then genRandomSubset g card
+                      else genRandom g
                   )
           -- coverage close to 1 would have extreme slowdown if we didn't do this
-          genRandomSubset :: Natural -> m [a]
-          genRandomSubset card = do
+          genRandomSubset :: StateGenM StdGen -> Natural -> StateT StdGen m [a]
+          genRandomSubset g card = do
             let dropNum = fromIntegral card - numInputs
             -- fill a hashtable with dropNum indices into exhaustiveGen output
             ht <- liftST $ HashTable.newSized inputSize
             let loop (-1) = pure ()
                 loop n = do
-                  input <- indexGen (fromIntegral card - 1)
+                  input :: Int <- uniformRM (0, fromIntegral card - 1) g
                   isNew <-
                     -- just abusing this mutable hashtable as a set
                     -- lookup and insert at the same time
@@ -227,12 +153,12 @@ benchSizes
             es' <- flip filterM es $ \(ix, _) ->
               liftST $ isNothing <$> HashTable.lookup ht ix
             pure $ snd <$> es'
-          genRandom :: m [a]
-          genRandom = do
+          genRandom :: StateGenM StdGen -> StateT StdGen m [a]
+          genRandom g = do
             ht <- liftST $ HashTable.newSized inputSize
             let loop 0 = pure []
                 loop n = do
-                  input <- randomGen inputSize
+                  input <- applyRandomGenM (randomGen inputSize) g
                   isNew <-
                     -- just abusing this mutable hashtable as a set
                     -- lookup and insert at the same time
