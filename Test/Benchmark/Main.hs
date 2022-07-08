242b8e9894e71ae4f8469a3277c6272e5614d8f3
Johannes Krause <johannes@mlabs.city>
2022-07-09 00:14:22 +0200

    (split)Benchmark-lib: Test.Benchmark.Main, benchmark-output gitignore

---
+++ Test/Benchmark/Main.hs
@@ -0,0 +1,53 @@
+module Test.Benchmark.Main (benchMain) where
+
+import Options.Applicative ((<**>))
+import Options.Applicative qualified as Opt
+import Path (Dir, Path, Rel, parseRelDir)
+import System.Directory (createDirectoryIfMissing)
+
+newtype Options = Options
+  { output :: FilePath
+  }
+
+outputOpt :: Opt.Parser FilePath
+outputOpt =
+  Opt.strOption
+    ( Opt.long "output-path"
+        <> Opt.short 'o'
+        <> Opt.metavar "OUTPUT_PATH"
+        <> Opt.value "./benchmark-output"
+        <> Opt.help "The output directory for writing benchmark results."
+    )
+
+benchOpt :: Opt.Parser Options
+benchOpt = Options <$> outputOpt
+
+parseOptions :: IO Options
+parseOptions = Opt.execParser p
+  where
+    p =
+      Opt.info
+        (benchOpt <**> Opt.helper)
+        ( Opt.fullDesc
+            <> Opt.progDesc "Run benchmarks, write results to output directory."
+        )
+
+-- | Passes the benchmark output directory to the argument.
+benchMain :: (Path Rel Dir -> IO ()) -> IO ()
+benchMain run = do
+  options <- parseOptions
+  -- This will thrown an error if it exists and is not a dir
+  -- True to create parent dirs
+  createDirectoryIfMissing True options.output
+  dir <- parseRelDir options.output
+  run dir
+
+{- TODO
+  The whole interface is rather low-level for now.
+  A high-level interface needs to:
+  - allow grouping benchmarks in a tree
+  - allow to filter the tree with command line arguments, just like Tasty/QuickCheck do
+  - needs to keep track of dependencies: a comparison of multiple
+    benchmark-outputs needs to make sure those benchmarks are run, no matter what
+    the filters were, but the benchmarks shouldn't be run more than once
+-}
