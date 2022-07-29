50f8225f66619ed335dfc2928bdff7fdc86e6374
Johannes Krause <johannes@mlabs.city>
2022-07-29 06:10:28 +0200

    (split)Benchmark-lib: Exhaustive: exhGenSets, fixed bugs, added tests

--- src/Test/Benchmark/Exhaustive.hs
+++ src/Test/Benchmark/Exhaustive.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 -- | Helpers for making exhaustive generators.
-module Test.Benchmark.Exhaustive (exhGenBags) where
+module Test.Benchmark.Exhaustive (exhGenBags, exhGenSets, cardSets) where
+
+import Acc (Acc, fromReverseList)
+import Control.Monad (join)
+import GHC.Exts (IsList (toList))
+import Numeric.SpecFunctions (choose)
+import Test.Benchmark.Sized (Cardinality (..))
 
 {- | Exhaustively generate lists (bags) of a given size.
 
@@ -14,10 +23,44 @@ exhGenBags ::
   -- | choice of unique elements (a set)
   [a] ->
   [[a]]
--- TODO should use dlist
-exhGenBags num [x] = pure $ replicate num x
-exhGenBags 0 _ = mempty
-exhGenBags num (x : xs) = do
-  reps <- [0 .. num]
-  (replicate reps x <>) <$> exhGenBags (num - reps) xs
-exhGenBags _ [] = error "no choice"
+exhGenBags num choice = map toList $ go num choice
+  where
+    go :: Int -> [a] -> [Acc a]
+    go num [x] = pure . fromReverseList $ replicate num x
+    go 0 _ = pure []
+    go num (x : xs) = do
+      reps <- [num, num - 1 .. 0]
+      (fromReverseList (replicate reps x) <>) <$> go (num - reps) xs
+    go _ [] = error "no choice"
+
+-- | Exhaustively generate lists (subsets) of a given size.
+exhGenSets ::
+  -- | number of elems to be generated
+  Int ->
+  -- | choice of unique elements (the superset)
+  [a] ->
+  [[a]]
+exhGenSets num choice = go num (length choice) choice
+  where
+    go 0 _ _ = pure []
+    go n c cs@(x : xs)
+      | n == c = pure cs
+      | n > c = err
+      | otherwise = do
+          join
+            [ (x :) <$> go (n - 1) (c - 1) xs
+            , go n (c - 1) xs
+            ]
+    go _ _ [] = err
+    err = error "requested more set elements than available"
+
+{- | Cardinality of list of subsets, given set size and size of superset
+
+ Use in conjunction with 'exhGenSets'.
+-}
+cardSets :: Int -> Int -> Cardinality
+cardSets n k =
+  -- 'choose' from 'math-functions' is accurate to 12 decimal places
+  if c > 10e12 then HugeCardinality else Cardinality $ round c
+  where
+    c = choose n k
---
+++ test/Main.hs
@@ -0,0 +1,86 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+module Main (main) where
+
+import Control.Monad (replicateM)
+import Control.Monad.Loops (iterateUntilM)
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IntSet
+import Data.List (find, sort)
+import Data.Maybe (isJust)
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
+import Test.Benchmark.Exhaustive (cardSets, exhGenBags, exhGenSets)
+import Test.Benchmark.Sized (Cardinality (..))
+import Test.QuickCheck (Gen, chooseInt, discard, expectFailure, forAll, sized, (===))
+import Test.Tasty (TestTree, adjustOption, defaultMain, testGroup)
+import Test.Tasty.QuickCheck (QuickCheckMaxSize, QuickCheckTests, testProperty)
+
+main :: IO ()
+main = do
+  setLocaleEncoding utf8
+  defaultMain . adjustOption go . testGroup "Properties" $
+    [ exhaustiveProps
+    ]
+  where
+    go :: QuickCheckTests -> QuickCheckTests
+    go = max 10000
+
+exhaustiveProps :: TestTree
+exhaustiveProps =
+  adjustOption (min (5 :: QuickCheckMaxSize)) $
+    testGroup
+      "Exhaustive"
+      [ testProperty "exhGenBags random containment" $
+          let gen = sized $ \size -> do
+                let range = testRange size
+                    choice = uncurry enumFromTo range
+                    pick n = replicateM n (chooseInt range)
+                bag <- sort <$> pick size
+                pure (size, choice, bag)
+              prop (size, choice, bag) =
+                let bags = exhGenBags size choice
+                 in isJust $ find (== bag) bags
+           in forAll gen prop
+      , testProperty "exhGenSets random containment" $
+          let gen = sized $ \size -> do
+                let range = testRange size
+                    choice = uncurry enumFromTo range
+                set <- IntSet.toList <$> genIntSet range size
+                pure (size, choice, set)
+              prop (size, choice, set) =
+                let sets = exhGenSets size choice
+                 in isJust $ find (== set) sets
+           in forAll gen prop
+      , testProperty "exhGenBags over-demand" $
+          expectFailure $ seq (exhGenBags 1 []) ()
+      , testProperty "exhGenSets over-demand" $
+          expectFailure $ seq (exhGenSets 1 []) ()
+      , testProperty "exhGenBags zero" $
+          exhGenBags 0 [1 :: Int] === [[]]
+      , testProperty "exhGenSets zero" $
+          exhGenSets 0 [1 :: Int] === [[]]
+      , testProperty "exhGenSets length" $
+          let gen = sized $ \size -> do
+                let range = testRange size
+                    choice = uncurry enumFromTo range
+                pure (size, choice)
+              prop (size, choice) =
+                let sets = exhGenSets size choice
+                    (lo, hi) = testRange size
+                    expectedCard = cardSets (hi - lo + 1) size
+                 in case expectedCard of
+                      HugeCardinality -> discard
+                      Cardinality card -> length sets === fromIntegral card
+           in forAll gen prop
+      ]
+
+-- | range used for testing
+testRange :: Int -> (Int, Int)
+testRange size = (1, size * 2)
+
+-- | range, size -> set
+genIntSet :: (Int, Int) -> Int -> Gen IntSet
+genIntSet range size = do
+  let genElem = chooseInt range
+      expand s = genElem >>= \e -> pure $ IntSet.insert e s
+  iterateUntilM (\s -> IntSet.size s == size) expand IntSet.empty
