608884725490e0cc36197e0ad69febbff6465276
Peter Dragos <peter@mlabs.city>
2022-08-02 17:14:04 -0400

    add mustCompile to the cabal and fix imports

--- src/Plutarch/Extra/Compile.hs
+++ src/Plutarch/Extra/Compile.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Plutarch.Extra.Compile (mustCompile) where
 
-import Data.Default (Default (Data))
+import Data.Default (Default (def))
+import qualified Data.Text as T
+import Plutarch (compile)
+import Plutarch.Prelude
+import PlutusLedgerApi.V2 (Script)
 
 -- | Compile a ClosedTerm, throwing an error if unsuccessful.
 mustCompile :: ClosedTerm a -> Script
