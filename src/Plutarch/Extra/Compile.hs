8ba1fcba253dd0a5444a5096d96d655eca0cf1f4
Hongrui Fang <chfanghr@gmail.com>
2022-08-05 20:44:43 +0800

    add version info of `mustCompile`

--- src/Plutarch/Extra/Compile.hs
+++ src/Plutarch/Extra/Compile.hs
@@ -7,7 +7,10 @@ import qualified Data.Text as T
 import Plutarch (ClosedTerm, compile)
 import PlutusLedgerApi.V1 (Script)
 
--- | Compile a ClosedTerm, throwing an error if unsuccessful.
+{- | Compile a ClosedTerm, throwing an error if unsuccessful.
+
+     @since 2.0.0
+-}
 mustCompile :: ClosedTerm a -> Script
 mustCompile t = case compile def t of
     Left err -> error $ unwords ["Plutarch compilation error:", T.unpack err]
