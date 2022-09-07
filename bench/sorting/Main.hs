ba500955a5749910c63c98771f7edde05cce6256
Koz Ross <koz.ross@retro-freedom.nz>
2022-09-07 14:46:59 +1200

    Better benchmarks

--- bench/sorting/Main.hs
+++ bench/sorting/Main.hs
@@ -8,7 +8,7 @@ import Data.Tagged (Tagged (Tagged))
 import Data.Text (unpack)
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import Plutarch (compile)
-import Plutarch.Evaluate (evalScriptHuge)
+import Plutarch.Evaluate (evalScriptHuge, evalTerm)
 import Plutarch.Extra.List (preplicate)
 import Plutarch.Extra.Ord (pfromOrd, pnubSort, pnubSortBy, preverseComparator)
 import PlutusCore.Evaluation.Machine.ExBudget (ExBudget (ExBudget))
@@ -49,38 +49,44 @@ benchNub :: (forall (s :: S). Term s PInteger) -> Int -> TestTree
 benchNub plen len =
     singleTest
         ("pnubSort, length " <> show len)
-        (ScriptBench $ pnubSort @PInteger @PList #$ preplicate # plen # 1)
+        (ScriptBench (pnubSort @PInteger @PList, preplicate # plen # 1))
 
 benchNub' :: (forall (s :: S). Term s PInteger) -> Int -> TestTree
 benchNub' plen len =
     singleTest
         ("pnubSort, length " <> show len)
-        (ScriptBench $ pnubSort @PInteger @PBuiltinList #$ preplicate # plen # 1)
+        (ScriptBench (pnubSort @PInteger @PBuiltinList, preplicate # plen # 1))
 
 benchNubCustom :: (forall (s :: S). Term s PInteger) -> Int -> TestTree
 benchNubCustom plen len =
     singleTest
         ("pnubSort, custom comparator, length " <> show len)
-        ( ScriptBench $
-            pnubSortBy @PInteger @PList # (preverseComparator # pfromOrd)
-                #$ preplicate # plen # 1
+        ( ScriptBench
+            ( pnubSortBy @PInteger @PList # (preverseComparator # pfromOrd)
+            , preplicate # plen # 1
+            )
         )
 
 -- Scaffolding and helpers
 
-newtype ScriptBench (a :: S -> Type) = ScriptBench (forall (s :: S). Term s a)
+newtype ScriptBench (a :: S -> Type) (b :: S -> Type)
+    = ScriptBench (ClosedTerm (b :--> a), ClosedTerm b)
 
-instance (Typeable a) => IsTest (ScriptBench a) where
-    run _ (ScriptBench t) _ = pure $ case compile def t of
-        Left err -> testFailed $ "Unexpected failure to compile: \n" <> unpack err
-        Right script -> case evalScriptHuge script of
-            (_, ExBudget (ExCPU cpu) (ExMemory mem), _) ->
-                testPassed $
-                    "CPU used: "
-                        <> commaSepThousands cpu
-                        <> "\nMemory used: "
-                        <> commaSepThousands mem
-                        <> "\n"
+instance (Typeable a, Typeable b) => IsTest (ScriptBench a b) where
+    run _ (ScriptBench (f, arg)) _ = pure $ case evalTerm def arg of
+        Left err -> testFailed $ "Argument unexpectedly failed to compile: \n" <> unpack err
+        Right (res, _, _) -> case res of
+            Left err -> testFailed $ "Argument unexpectedly failed to evaluate: \n" <> show err
+            Right arg' -> case compile def (f # arg') of
+                Left err -> testFailed $ "Function unexpectedly failed to compile: \n" <> show err
+                Right script -> case evalScriptHuge script of
+                    (_, ExBudget (ExCPU cpu) (ExMemory mem), _) ->
+                        testPassed $
+                            "CPU used: "
+                                <> commaSepThousands cpu
+                                <> "\nMemory used: "
+                                <> commaSepThousands mem
+                                <> "\n"
     testOptions = Tagged []
 
 commaSepThousands :: CostingInteger -> String
