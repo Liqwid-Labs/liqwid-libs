75e06d27f7ce8d973c5437225c22bff69cb9f111
Koz Ross <koz.ross@retro-freedom.nz>
2022-10-14 13:10:50 +1300

    Fix formatting again

--- src/Plutarch/Test/QuickCheck.hs
+++ src/Plutarch/Test/QuickCheck.hs
@@ -379,7 +379,7 @@ instance
           Nothing -> property True
       Right (Right p', _, t) ->
         case h of
-          Just h' -> haskEquiv @e @'ByComplete h' (TestableTerm p') Nil
+          Just h' -> haskEquiv @e @('ByComplete) h' (TestableTerm p') Nil
           Nothing -> failWith $ "Haskell expected failure, but Plutarch succeed.\n" <> show t
 
 -- | @since 2.1.0
@@ -409,8 +409,8 @@ instance
   HaskEquiv 'OnBoth 'ByComplete h p '[]
   where
   haskEquiv h p _ =
-    haskEquiv @'OnPEq  @'ByComplete h p Nil
-      .&&. haskEquiv @'OnPData  @'ByComplete h p Nil
+    haskEquiv @('OnPEq)  @('ByComplete) h p Nil
+      .&&. haskEquiv @('OnPData)  @('ByComplete) h p Nil
 
 {- | Simplified version of `haskEquiv`. It will use arbitrary instead of
      asking custom generators.
