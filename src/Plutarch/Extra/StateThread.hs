0996aafa35c40182218c80bf990cf0971d598ae1
Peter Dragos <peter@mlabs.city>
2022-10-25 12:42:56 -0400

    renamings, comments, and formatting

--- src/Plutarch/Extra/StateThread.hs
+++ src/Plutarch/Extra/StateThread.hs
@@ -1,6 +1,6 @@
 module Plutarch.Extra.StateThread (
   withStateThread,
-  withStateThread',
+  pwithStateThread,
 ) where
 
 import Plutarch.Api.V1 (PCurrencySymbol, PValue)
@@ -16,17 +16,18 @@ import Plutarch.Api.V2 (
 import Plutarch.Extra.Field (pletAll)
 
 {- | Adds a state thread to a minting policy.
+ Parameterized at the Haskell level.
 
  @since 3.11.0
 -}
-withStateThread' ::
+withStateThread ::
   forall (s :: S).
   -- | Minting policy to wrap
   Term s PMintingPolicy ->
   -- | Initial spend
   Term s PTxOutRef ->
   Term s PMintingPolicy
-withStateThread' mp ref = plam $ \red ctx -> pletAll ctx $ \ctx' ->
+withStateThread mp ref = plam $ \red ctx -> pletAll ctx $ \ctx' ->
   pletFields @'["inputs", "mint"] (getField @"txInfo" ctx') $ \txInfo ->
     pmatch (getField @"purpose" ctx') $ \case
       PMinting thisPolicy ->
@@ -40,14 +41,15 @@ withStateThread' mp ref = plam $ \red ctx -> pletAll ctx $ \ctx' ->
           (ptraceError "stateThread: Not minting a unique state token")
       _ -> ptraceError "stateThread: Not a minting script purpose"
 
+{- | Adds a state thread to a minting policy.
+ Parameterized at the Plutarch level
 
--- | Adds a state thread to a minting policy
---
--- @since 3.11.0
-withStateThread ::
+ @since 3.11.0
+-}
+pwithStateThread ::
   forall (s :: S).
   Term s (PMintingPolicy :--> PTxOutRef :--> PMintingPolicy)
-withStateThread = plam withStateThread'
+pwithStateThread = plam withStateThread'
 
 -- Helpers
 
