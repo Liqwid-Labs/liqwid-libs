02598dfb7ef1f2e3d46f532ea1f0377eedf8e5ab
Seungheon Oh <seungheon.ooh@gmail.com>
2022-11-02 20:40:53 -0500

    add `RoledScript`

--- export-stat/Main.hs
+++ export-stat/Main.hs
@@ -1,18 +1,14 @@
-import Optics (view)
-import System.Exit (die)
+import Codec.Serialise (serialise)
+import Control.Applicative ((<|>))
 import Data.Aeson qualified as Aeson
-import System.Environment (getArgs, getProgName)
-import System.FilePath (takeBaseName)
-import GHC.Generics qualified as GHC
+import Data.Aeson.Encode.Pretty (encodePretty)
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as LBS
 import Data.ByteString.Short qualified as SBS
-import ScriptExport.ScriptInfo (RawScriptExport, ScriptExport)
 import Data.Map (Map, fromList, toList)
 import Data.Text (Text)
-import Control.Applicative ((<|>))
-import Data.Aeson.Encode.Pretty (encodePretty)
-import Codec.Serialise (serialise)
+import GHC.Generics qualified as GHC
+import Optics (view)
 import Plutarch.Evaluate (evalScript)
 import PlutusLedgerApi.V2 (
   ExBudget (ExBudget),
@@ -20,16 +16,19 @@ import PlutusLedgerApi.V2 (
   ExMemory (..),
   Script,
  )
-import Ply (TypedScriptEnvelope(tsScript))
+import Ply (TypedScriptEnvelope (tsScript))
+import ScriptExport.ScriptInfo (RawScriptExport, ScriptExport)
+import System.Environment (getArgs, getProgName)
+import System.Exit (die)
+import System.FilePath (takeBaseName)
 
 data FileFormats
   = Raw RawScriptExport
   | Export (ScriptExport ())
   | MultipleExport (Map Text (ScriptExport ()))
-  deriving stock Show
+  deriving stock (Show)
 
-data ScriptStats
-  = ScriptStats
+data ScriptStats = ScriptStats
   { size :: Int
   , cpuBudget :: ExCPU
   , memBudget :: ExMemory
@@ -48,30 +47,34 @@ decodeExport filename = do
 
   return $
     Raw <$> Aeson.eitherDecodeStrict content
-    <|> Export <$> Aeson.eitherDecodeStrict content
-    <|> MultipleExport <$> Aeson.eitherDecodeStrict content
+      <|> Export <$> Aeson.eitherDecodeStrict content
+      <|> MultipleExport <$> Aeson.eitherDecodeStrict content
 
 makeStats :: FileFormats -> Map Text ScriptStats
 makeStats =
   \case
-    Raw e -> (go . tsScript) <$> view #rawScripts e
-    Export e -> go <$> view #scripts e
+    Raw e -> go . tsScript <$> view #rawScripts e
+    Export e -> go . view #script <$> view #scripts e
     MultipleExport e ->
       fromList $
-        foldMap (\(k, x) -> (\(k', x') -> (k <> "-" <> k', go x')) <$>
-                  toList (view #scripts x)) $ toList e
+        foldMap
+          ( \(k, x) ->
+              (\(k', x') -> (k <> "-" <> k', go $ view #script x'))
+                <$> toList (view #scripts x)
+          )
+          $ toList e
   where
     go :: Script -> ScriptStats
     go script =
       let (_res, ExBudget cpu mem, _traces) = evalScript script
           size = SBS.length . SBS.toShort . LBS.toStrict . serialise $ script
-      in ScriptStats size cpu mem
+       in ScriptStats size cpu mem
 
 main :: IO ()
 main = do
   exportFile <-
     getArgs >>= \case
-      (x: _) -> return x
+      (x : _) -> return x
       _ -> help
 
   exportContent' <- decodeExport exportFile
