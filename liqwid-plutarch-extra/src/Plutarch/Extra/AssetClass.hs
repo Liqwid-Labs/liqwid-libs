8dcb59a938a8bb555928aa7ba91f0c3c5fe6394a
Hongrui Fang <chfanghr@gmail.com>
2023-03-28 21:30:08 +0800

    add `PTryFrom PData (PAsData PAssetClassData)` instance

--- liqwid-plutarch-extra/src/Plutarch/Extra/AssetClass.hs
+++ liqwid-plutarch-extra/src/Plutarch/Extra/AssetClass.hs
@@ -318,6 +318,8 @@ instance DerivePlutusType PAssetClassData where
 instance Plutarch.Lift.PUnsafeLiftDecl PAssetClassData where
   type PLifted PAssetClassData = AssetClass
 
+instance PTryFrom PData (PAsData PAssetClassData)
+
 -- | @since 3.10.0
 passetClassData ::
   forall (s :: S).
