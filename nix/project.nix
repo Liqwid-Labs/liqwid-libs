539c2dec60a305c2ef32e9dd2e83c61d17a9e28c
edgr <edgr@pm.me>
2024-06-04 13:59:58 +0200

    Bump Ply

--- nix/project.nix
+++ nix/project.nix
@@ -7,10 +7,10 @@ pkgs.haskell-nix.cabalProject'
   inputMap = {
     "https://chap.intersectmbo.org/" = inputs.CHaP;
     "https://github.com/Plutonomicon/plutarch-plutus.git" = inputs.plutarch;
-    "https://github.com/mlabs-haskell/ply.git" = inputs.ply;
+    "https://github.com/the-headless-ghost/ply.git" = inputs.ply;
   };
   sha256map = {
     "https://github.com/Plutonomicon/plutarch-plutus"."a170ce4763891e24f59572209722edf2874a7d83" = "sha256-ZIKSI6zVmBd6trzx7W46sshTv+oLW97FnPERAnBlhhc=";
-    "https://github.com/mlabs-haskell/ply"."efeb0baddffccdbbf38d1e7e02cd1778422eeb78" = "sha256-om9EEGrw09gm0i+i9GYp0KYWQMXAd6ZvAZPzW6cGSLw=";
+    "https://github.com/the-headless-ghost/ply"."7c77122e040ec34ced500946950f9041cb05277e" = "sha256-dSXC40u8taswmygFDuGDLkdRj/ebsnk/hsJM5JAmWE4=";
   };
 }
